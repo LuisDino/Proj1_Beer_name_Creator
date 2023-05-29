577c7c5145032533ca39092ae54d8ae683f0375a
Luis DN <118082702+LuisDino@users.noreply.github.com>
2023-05-29 18:42:27 -0300

    Update proj1.py

--- proj1.py
+++ proj1.py
@@ -1,1 +1,9 @@
+print('*'*40)
+print('            CREADOR DE NOMBRES')
+print('*'*40)
+print('\n')
+Q1=input('¿ Cual es tu ciudad o pais preferido?: ')
+Q2=input('¿Cual es tu color preferido?: ')
+print('*'*40)
+print(f"El nombre de tu cerverza es : {Q1} {Q2}")
 
